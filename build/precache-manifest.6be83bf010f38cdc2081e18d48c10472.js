dfe9b50b8224cea2c3e9b1fdf8bdecf461f36d41
brunoreyes <bruno619reyes@gmail.com>
2020-10-08 11:07:54 -0500

    deployed app to heroku and added database

---
+++ build/precache-manifest.6be83bf010f38cdc2081e18d48c10472.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5283237efacfe468b07ce4ab400c056b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a8eba8363f3fd7cdc14",
+    "url": "/static/css/main.a0b87b20.chunk.css"
+  },
+  {
+    "revision": "b7d0eea377859e2c3282",
+    "url": "/static/js/2.88292ed9.chunk.js"
+  },
+  {
+    "revision": "2043c2e0a3c0cccc54faedbb61de9be3",
+    "url": "/static/js/2.88292ed9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a8eba8363f3fd7cdc14",
+    "url": "/static/js/main.6900f8bd.chunk.js"
+  },
+  {
+    "revision": "4b729c205ca4519f9ff9",
+    "url": "/static/js/runtime-main.a89d20e0.js"
+  }
+]);
